63f0281245a298a76afca93217b29e3885628ecc
Dmitrii <MY_NAME@example.com>
2021-03-19 21:23:58 -0700

    first test

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,14 +1,14 @@
 import LoginPage from  '../pageobjects/login.page';
-import SecurePage from '../pageobjects/secure.page';
+import ProfilePage from '../pageobjects/profile.page';
 
-describe('My Login application', () => {
-    it('should login with valid credentials', () => {
+
+describe('Auth', () => {
+    it('user logs in with valid data', () => {
         LoginPage.open();
-
-        LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        expect(SecurePage.flashAlert).toBeExisting();
-        expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
+        LoginPage.setLogin('speldima@inbox.ru');
+        LoginPage.setPassword('181075zxcvb');
+        LoginPage.clickSubmitButton();
+        ProfilePage.isOpen();
     });
 });
 
